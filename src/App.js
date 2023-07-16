a53f970f0ff76cca429c9343daf259fee083e7dc
sleepy2535 <greenapple5657@gmail.com>
2023-07-16 23:08:52 +0900

    second-grade-hyeonjoo 브랜치 생성

--- src/App.js
+++ src/App.js
@@ -1,30 +1,25 @@
-
-import main from './pages/main';
-import './App.css';
-import GraduateList from './pages/developers/GraduateList';
+import main from "./pages/main";
+import "./App.css";
+import GraduateList from "./pages/developers/GraduateList";
 import Main from "./pages/introduce/pages/Main";
-import Member from './pages/developers/Member';
+import Member from "./pages/developers/Member";
 
 import React, { useState } from "react";
 
 import { BrowserRouter, Route, Routes, Switch, Link } from "react-router-dom";
-import arrayprc2 from './pages/projects/arrayprc2';
+import arrayprc2 from "./pages/projects/arrayprc2";
 
 function App() {
-  return (
-    <>
-      <switch>
-      <Route path = "/" exact component = {main}/>
-      <Route path = "/introduce" exact component = {Main}/>
-      <Route path ="/projects" exact component = {arrayprc2}/>
-      <Route path="/developers" exact component= {GraduateList} />
-
-      </switch>
-
-    </>
-    
-  
-  );
+    return (
+        <>
+            <switch>
+                <Route path="/" exact component={main} />
+                <Route path="/introduce" exact component={Main} />
+                <Route path="/projects" exact component={arrayprc2} />
+                <Route path="/developers" exact component={GraduateList} />
+            </switch>
+        </>
+    );
 }
 
 export default App;
